24500c439b02ddf89cd4ca20d7bd438fb211647d
W8gReZW <127243300+W8gReZW@users.noreply.github.com>
2023-03-18 14:22:04 -0400

    update marquee fix on mobile

--- start_with_db.sh
+++
@@ -1,6 +0,0 @@
-export DATABASE_URL="postgres://user:password@localhost/dbname"
-
-# Activate your virtual environment (if using one)
-# source /path/to/your/virtualenv/bin/activate
-
-python manage.py runserver
