48ed25de6e1a4b35f72f087e2a07155fb8607367
W8gReZW <127243300+W8gReZW@users.noreply.github.com>
2023-03-19 01:44:11 -0400

    commas

--- pingojo/settings.py
+++ pingojo/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    "django.contrib.humanize",
     "website",
     "allauth",
     "allauth.account",
