03ab5fb57e0897bac53287fa00d48e873dfde227
W8gReZW <127243300+W8gReZW@users.noreply.github.com>
2023-07-05 16:10:38 -0400

    update cors

--- pingojo/settings.py
+++ pingojo/settings.py
@@ -105,7 +105,7 @@ CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST = [
     "https://boards.greenhouse.io",
     "https://boards.eu.greenhouse.io",
     "https://wellfound.com",
-    "https://pythoncodingjobs.com/"
+    "https://pythoncodingjobs.com"
 ]
 
 ROOT_URLCONF = "pingojo.urls"
