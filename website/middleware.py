9e2c74ceb63464e4140080a0d7ff4250878a4b39
W8gReZW <127243300+W8gReZW@users.noreply.github.com>
2023-07-07 12:39:45 -0400

    try this for csrf

--- website/middleware.py
+++ website/middleware.py
@@ -1,7 +1,8 @@
 
 from django.shortcuts import render
-
 import re
+from django.http import HttpResponseForbidden
+from urllib.parse import urlparse
 from django.http import HttpResponseForbidden
 
 class CustomMiddleware:
@@ -13,22 +14,34 @@ class CustomMiddleware:
         return render(request, 'website/loading_page.html')
     
 
+
+
 class CustomCsrfMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
 
-        referer = request.META.get('HTTP_REFERER')
-        if referer is not None and not re.match(r'https://\w+\.applytojob\.com', referer):
-            # If the referer is set and does not match the trusted pattern, return a 403 response.
-            return HttpResponseForbidden()
+        if request.method == 'POST':
+            referer = request.META.get('HTTP_REFERER')
+            if referer is not None:
+                referer_hostname = urlparse(referer).hostname
+                allowed_origins = [
+                    "mail.google.com",
+                    "pingojo.com",
+                    "localhost",
+                    "localhost:8000",
+                    "127.0.0.1",
+                    "127.0.0.1:8000",
+                    "boards.greenhouse.io",
+                    "boards.eu.greenhouse.io",
+                    "wellfound.com",
+                    "pythoncodingjobs.com",
+                ]
+
+                if referer_hostname not in allowed_origins and not re.match(r'https://\w+\.applytojob\.com', referer):
+                    return HttpResponseForbidden()
 
         response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
 
         return response
