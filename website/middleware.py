ae3ee920ecab197d58cf425ba6046b85441962ea
QA2A <150867722+QA2A@users.noreply.github.com>
2024-08-07 21:22:16 -0400

    Create middleware.py

---
+++ website/middleware.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponseForbidden
+
+
+class BlockUserAgentsMiddleware:
+    # List of blocked user-agent strings
+    BLOCKED_USER_AGENTS = ["DotBot", "SemrushBot"]
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user_agent = request.META.get('HTTP_USER_AGENT', '')
+
+        for blocked_agent in self.BLOCKED_USER_AGENTS:
+            if blocked_agent in user_agent:
+                return HttpResponseForbidden()  # Reject the request with a 403 Forbidden status
+
+        response = self.get_response(request)
+        return response
